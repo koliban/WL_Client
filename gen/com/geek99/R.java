0142904e87673dbedc543a77a032d023174d0a58
chenhaonan <1004643191@qq.com>
2018-06-18 15:40:33 +0800

    1.12

--- gen/com/geek99/R.java
+++ gen/com/geek99/R.java
@@ -24,27 +24,33 @@ public final class R {
         public static final int zhuxiao=0x7f02000a;
     }
     public static final class id {
-        public static final int button1=0x7f060004;
-        public static final int diancai_button1=0x7f060008;
-        public static final int editText1=0x7f060001;
-        public static final int editText2=0x7f060002;
-        public static final int gridview=0x7f060005;
-        public static final int imageView1=0x7f06000c;
-        public static final int listView1=0x7f060007;
-        public static final int num_editText1=0x7f06000a;
-        public static final int order_button2=0x7f060009;
-        public static final int spinner1=0x7f060006;
-        public static final int table_gridview=0x7f06000b;
-        public static final int textView1=0x7f060000;
+        public static final int button1=0x7f060007;
+        public static final int desc_editText2=0x7f060004;
+        public static final int diancai_button1=0x7f06000d;
+        public static final int editText1=0x7f060005;
+        public static final int editText2=0x7f060006;
+        public static final int gridview=0x7f060008;
+        public static final int id_textView1=0x7f060009;
+        public static final int imageView1=0x7f060010;
+        public static final int listView1=0x7f06000c;
+        public static final int name_textView1=0x7f06000b;
+        public static final int num_editText1=0x7f060002;
+        public static final int num_textView1=0x7f06000a;
+        public static final int order_button2=0x7f06000e;
+        public static final int spinner1=0x7f060000;
+        public static final int table_gridview=0x7f06000f;
+        public static final int textView1=0x7f060001;
         public static final int textView2=0x7f060003;
     }
     public static final class layout {
-        public static final int login=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int order=0x7f030002;
-        public static final int table=0x7f030003;
-        public static final int table_item=0x7f030004;
-        public static final int update=0x7f030005;
+        public static final int diancai=0x7f030000;
+        public static final int login=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int menu_item=0x7f030003;
+        public static final int order=0x7f030004;
+        public static final int table=0x7f030005;
+        public static final int table_item=0x7f030006;
+        public static final int update=0x7f030007;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
